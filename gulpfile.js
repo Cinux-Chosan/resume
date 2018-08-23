c0c547723e213f71125001bdb11e3d86d1371c89
chosan <179817004@qq.com>
2018-08-23 16:23:42 +0800

    consummated

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,10 @@
 const gulp = require("gulp");
 const less = require("gulp-less");
-const handlebars = require("gulp-handlebars");
-const wrap = require("gulp-wrap");
-const declare = require("gulp-declare");
-const concat = require("gulp-concat");
-var defineModule = require("gulp-define-module");
-
-// gulp.task("templates", () => {
-//   gulp
-//     .src(["src/templates/**/*.hbs"])
-//     .pipe(handlebars())
-//     .pipe(defineModule("node"))
-//     .pipe(gulp.dest("build/templates/"));
-// });
-
-// gulp.task("components", () => {
-//   gulp.src('')
-// })
 
 gulp.task("html", () => {
   gulp.src("*.html").pipe(gulp.dest("dist"));
 });
+
 gulp.task("less", () => {
   gulp
     .src("less/*.less")
@@ -31,6 +15,7 @@ gulp.task("less", () => {
 gulp.task("static", () => {
   gulp.src("static/**/*").pipe(gulp.dest("dist/static"));
 });
+
 gulp.task("default", ["html", "less", "static"], () => {
   gulp.watch("less/*.less", ["less"]);
   gulp.watch("*.html", ["html"]);
