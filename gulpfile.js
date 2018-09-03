4d04d948cf5fd8cd7853470d5f2ed44d57f7079c
Your Name <you@example.com>
2018-09-03 18:19:39 +0800

    ..

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,34 @@
 const gulp = require("gulp");
 const less = require("gulp-less");
+const autoprefixer = require('gulp-autoprefixer');
+const uglifycss = require('gulp-uglifycss');
+const htmlmin = require('gulp-htmlmin');
+const hash = require('gulp-hash');
 
 gulp.task("html", () => {
   gulp
     .src(["**/*.html", "!{dist,node_modules}/**/*.html"])
+    .pipe(hash({
+      deleteOld: true
+    }))
+    .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest("dist"));
 });
 
 gulp.task("less", () => {
   gulp
     .src(["**/*.less", "!{dist,node_modules}/**/*.less"])
-    .pipe(less())
+    .pipe(hash({
+      deleteOld: true
+    }))    .pipe(less())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],  // https://github.com/browserslist/browserslist#queries
+      cascade: false
+    }))
+    .pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
     .pipe(gulp.dest("dist"));
 });
 
