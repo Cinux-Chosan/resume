c26e0d6613bf57778d47dd278190645f89edb2d7
Your Name <you@example.com>
2018-09-04 00:43:03 +0800

    ..

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,32 @@ const less = require("gulp-less");
 const autoprefixer = require('gulp-autoprefixer');
 const uglifycss = require('gulp-uglifycss');
 const htmlmin = require('gulp-htmlmin');
-const hash = require('gulp-hash');
-
-gulp.task("html", () => {
-  gulp
-    .src(["**/*.html", "!{dist,node_modules}/**/*.html"])
-    .pipe(hash({
-      deleteOld: true
-    }))
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest("dist"));
-});
+const rev = require('gulp-rev');
+const through = require('through2');
+const fs = require('fs');
+const path = require('path');
 
 gulp.task("less", () => {
-  gulp
+  return gulp
     .src(["**/*.less", "!{dist,node_modules}/**/*.less"])
-    .pipe(hash({
-      deleteOld: true
-    }))    .pipe(less())
+    .pipe(rev())
+    .pipe(less())
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],  // https://github.com/browserslist/browserslist#queries
       cascade: false
     }))
-    .pipe(uglifycss({
-      "maxLineLen": 80,
-      "uglyComments": true
+    .pipe(uglifycss())
+    .pipe(rev.manifest({ // rev.manifest 有 bug， 它只返回了 rev-manifest.json 文件留，其它都丢失，因此需要放在 dest 后
+      merge: true
     }))
+    .pipe(gulp.dest("dist"))
+});
+
+gulp.task("html", ['less'], () => {
+  gulp
+    .src(["**/*.html", "!{dist,node_modules}/**/*.html"])
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(replace())
     .pipe(gulp.dest("dist"));
 });
 
@@ -36,8 +36,27 @@ gulp.task("static", () => {
   gulp.src("static/**/*").pipe(gulp.dest("dist/static"));
 });
 
-gulp.task("default", ["html", "less", "static"], () => {
-  gulp.watch(["**/*.less", "!{dist,node_modules}/**/*.less"], ["less"]);
-  gulp.watch(["**/*.html", "!{dist,node_modules}/**/*.html"], ["html"]);
+gulp.task("default", ["html", "static"], () => {
+  // gulp.watch(["**/*.less", "!{dist,node_modules}/**/*.less"], ["less"]);
+  gulp.watch(["**/*.{html,less}", "!{dist,node_modules}/**/*.{html,less}"], ["html"]);
 });
 
+function replace() {
+  const { dirname, basename } = path;
+  const conf = JSON.parse(fs.readFileSync('dist/rev-manifest.json'));
+  return through.obj(function (file, enc, cb) {
+    if (file.isBuffer()) {
+      for (const origin in conf) {
+        const revPath = conf[origin];
+        if (conf.hasOwnProperty(origin) &&
+          dirname(file.path).endsWith(dirname(origin)) &&
+          basename(revPath).endsWith('.css')
+        ) {
+          file.contents = Buffer.from(file.contents.toString().replace(basename(origin, 'less'), basename(revPath, 'css')));
+        }
+      }
+    }
+    this.push(file);
+    return cb();
+  });
+}
