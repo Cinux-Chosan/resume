55ffdd4208568e73f345e14b510cd877dffc1ff8
Your Name <you@example.com>
2018-09-04 02:28:38 +0800

    ..

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,20 @@ const rev = require('gulp-rev');
 const through = require('through2');
 const fs = require('fs');
 const path = require('path');
+const rimraf = require('rimraf');
 
 gulp.task("less", () => {
   return gulp
     .src(["**/*.less", "!{dist,node_modules}/**/*.less"])
-    .pipe(rev())
     .pipe(less())
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],  // https://github.com/browserslist/browserslist#queries
-      cascade: false
     }))
     .pipe(uglifycss())
-    .pipe(rev.manifest({ // rev.manifest 有 bug， 它只返回了 rev-manifest.json 文件留，其它都丢失，因此需要放在 dest 后
-      merge: true
-    }))
-    .pipe(gulp.dest("dist"))
+    .pipe(rev())
+    .pipe(gulp.dest("dist"))  // 写入处理过后的文件
+    .pipe(rev.manifest())
+    .pipe(gulp.dest("dist"))  // 写入 rev-manifest.json 文件
 });
 
 gulp.task("html", ['less'], () => {
@@ -41,6 +40,16 @@ gulp.task("default", ["html", "static"], () => {
   gulp.watch(["**/*.{html,less}", "!{dist,node_modules}/**/*.{html,less}"], ["html"]);
 });
 
+gulp.task("del", () => {
+  return new Promise(res => {
+    rimraf('dist', () => {
+      res();
+    });
+  }) 
+})
+
+gulp.task("b", ['del', 'html'])
+
 function replace() {
   const { dirname, basename } = path;
   const conf = JSON.parse(fs.readFileSync('dist/rev-manifest.json'));
@@ -52,7 +61,7 @@ function replace() {
           dirname(file.path).endsWith(dirname(origin)) &&
           basename(revPath).endsWith('.css')
         ) {
-          file.contents = Buffer.from(file.contents.toString().replace(basename(origin, 'less'), basename(revPath, 'css')));
+          file.contents = Buffer.from(file.contents.toString().replace(basename(origin), basename(revPath)));
         }
       }
     }
