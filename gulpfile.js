5f96efd47944a1cfb16c2b6354698d089cfb8897
Your Name <you@example.com>
2018-09-09 02:00:51 +0800

    preDeployLocal

--- gulpfile.js
+++ gulpfile.js
@@ -79,10 +79,10 @@ async function createPDF() {
 }
 
 function replace() {
-  const conf = JSON.parse(fs.readFileSync('dist/rev-manifest.json'));
+  const conf = JSON.parse(fs.readFileSync('dist/rev-manifest.json') || {});
   return through.obj(function (file, enc, cb) {
     if (file.isBuffer()) {
-      let strFileContents = file.contents.toString().replace('{{globalTimeFormat}}', `来自 https://chosan.cn/resume/v2/resume.html ${new Date().toLocaleDateString()}`)
+      let strFileContents = file.contents.toString().replace('{{pageFooterText}}', `来自 https://chosan.cn/resume/v2/resume.html ${new Date().toLocaleDateString()}`)
       for (const origin in conf) {
         const revPath = conf[origin];
         const revBase = basename(revPath);
